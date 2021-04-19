2acd3b4dc361dff32bd69d13b0e9c0a1d4bada7b
JoseXS <xskunk@gmail.com>
2021-04-19 14:28:51 +0200

    Nuevos cmabios de diseño
    Signed-off-by: JoseXS <xskunk@gmail.com>

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -4,23 +4,28 @@ import { AdminDashboardPage } from './dashboard/admin-dashboard.page';
 import { AdminListAllPage } from './list-all/admin-list-all.page';
 import { AdminListSentPage } from './list-sent/admin-list-sent.page';
 import { AdminEditQuestionPage } from './edit-question/admin-edit-question.page';
+import { AdminGuard } from 'app/guards/admin.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminDashboardPage,
+    canActivate: [AdminGuard],
   },
   {
     path: 'sent',
     component: AdminListSentPage,
+    canActivate: [AdminGuard],
   },
   {
     path: 'question',
     component: AdminEditQuestionPage,
+    canActivate: [AdminGuard],
   },
   {
     path: 'all',
     component: AdminListAllPage,
+    canActivate: [AdminGuard],
   },
 ];
 
