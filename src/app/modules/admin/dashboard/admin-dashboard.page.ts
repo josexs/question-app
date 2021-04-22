54d4f90305655f7c675d04b11cebf36f3dab85a8
JoseXS <xskunk@gmail.com>
2021-04-23 00:34:26 +0200

    Cambios de diseño en admin
    Signed-off-by: JoseXS <xskunk@gmail.com>

--- src/app/modules/admin/dashboard/admin-dashboard.page.ts
+++ src/app/modules/admin/dashboard/admin-dashboard.page.ts
@@ -7,10 +7,56 @@ import { Router } from '@angular/router';
   templateUrl: 'admin-dashboard.page.html',
 })
 export class AdminDashboardPage {
+  items = [];
   constructor(private router: Router, private storageProvider: StorageProvider) {}
 
+  async ionViewWillEnter(): Promise<void> {
+    this.createItems();
+  }
+
+  createItems() {
+  this.items = [
+    {
+      title: 'Todas las preguntas',
+      subtitle: 'Solo las publicadas',
+      action: 'start',
+      color: 'dark',
+      state: true,
+      event: 'adminAllQuestions',
+      route: '/admin/all',
+    },
+    {
+      title: 'Preguntas enviadas',
+      subtitle: 'Solo las enviadas',
+      action: 'continue',
+      color: 'dark',
+      state: true,
+      event: 'adminSentQuestions',
+      route: '/admin/sent',
+    },
+    {
+      title: 'Crear pregunta',
+      subtitle: 'Añade directamente preguntas',
+      action: 'continue',
+      color: 'dark',
+      state: true,
+      event: 'adminCreateQuestion',
+      route: '/admin/create',
+    },
+    {
+      title: 'Cerrar sesion',
+      subtitle: 'Salir del menu admin',
+      action: 'continue',
+      color: 'dark',
+      state: true,
+      event: 'adminLogout',
+      route: '/',
+    },
+  ];
+}
+
   goTo(route: string) {
-    this.router.navigate([`admin/${route}`]);
+    this.router.navigate([`${route}`]);
   }
 
   logout() {
